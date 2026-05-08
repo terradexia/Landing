d9b314776f472aa6199f10fdc63580b602356c44
terradexia <terradexia@gmail.com>
2026-05-08 03:24:57 -0300

    Update lead.js

--- api/lead.js
+++ api/lead.js
@@ -31,10 +31,5 @@ export default async function handler(req, res) {
   });
 
   const data = await response.json();
-
-  if (!response.ok) {
-    return res.status(500).json({ error: data });
-  }
-
-  return res.status(200).json({ ok: true });
+  return res.status(200).json({ ok: response.ok, airtable: data });
 }
